e520e6c446cc3175660e7f5b09c84ec52417bdda
EliLePissenlit <e.clement1002@gmail.com>
2024-10-17 17:54:05 +0200

    a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function afficherIndice() {
+function afficherrIndice() {
     var indiceBloc = document.createElement('p');
     indiceBloc.innerText = "L'indice est: Des lumières scintillantes dans le ciel nocturne!";
     indiceBloc.classList.add('indice');
